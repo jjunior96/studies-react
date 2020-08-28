d7003edb3cdc85b966481c503b69e5f46853b31f
Junior <jjalves96@gmail.com>
2020-08-28 09:18:21 -0300

    feat: create Global Styles

---
+++ beginner/repo-github/src/styles/global.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    min-height: 100%;
+  }
+
+  body {
+    background-color: #7159c1;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    color: #222;
+    font-size: 14px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
